076eac5d8286b7ed729b54e759eebc4beff79c70
samirarasouli73 <148463681+samirarasouli73@users.noreply.github.com>
2023-11-15 13:15:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RemindersPlus
+ New Reminder Test
---
+++ RemindersPlus/RemindersPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersPlusApp.swift
+//  RemindersPlus
+//
+//  Created by Samira Resouliikhechi on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
